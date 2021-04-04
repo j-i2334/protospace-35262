e62bcde9e7c6035b1dff3045ed5e855bba8f9cef
j-i2334 <80821404+j-i2334@users.noreply.github.com>
2021-04-04 18:08:34 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'prototypes/index'
+  # post 'prototypes/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "prototypes#index"
+  resources :users, only: [:show]
+  resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :comments, only: [:index, :create, :destroy]
+  end
+end
